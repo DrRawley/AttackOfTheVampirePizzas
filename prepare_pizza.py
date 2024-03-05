2e718a180d130281ddd0c731effa6a425866911e
DrRawley <rawley.greene@gmail.com>
2024-03-04 19:19:25 -0500

    play around with drawing shapes

--- prepare_pizza.py
+++ prepare_pizza.py
@@ -16,11 +16,18 @@ WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
 GAME_WINDOW = display.set_mode(WINDOW_RES) #tuple data type
 display.set_caption('Attack of the Vampire Pizzas!')
 
+#draw pizza box
+draw.rect(GAME_WINDOW, (160,82,45), (895,395,110,110), 5)
+draw.rect(GAME_WINDOW, (160,82,45), (895,295,110,110), 0) #lid
 #Setup enemy image
 pizza_img = image.load('gameassets/vampire.png')
 pizza_surf = Surface.convert_alpha(pizza_img)
 VAMPIRE_PIZZA = transform.scale(pizza_surf, (100,100))
 GAME_WINDOW.blit(VAMPIRE_PIZZA, (900,400))
+
+#Add a giant pepperoni
+draw.circle(GAME_WINDOW, (255,0,0), (925,425), 25, 0)
+
 
 #-----------------------------------------------------------------
 #Start Main Game Loop
